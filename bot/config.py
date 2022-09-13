77950f971f91363ed194d3c583170b2fa6e7271c
Vaibhav ka ultroid <97387093+Awesome-Vaibhav@users.noreply.github.com>
2022-09-14 00:06:29 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -18,7 +18,7 @@ from decouple import config
 try:
     APP_ID = "7556932"
     API_HASH = "6b99e012069f373abbcac581d3cdd6db"
-    BOT_TOKEN = "5685905647:AAGbEg4hPS6Viw5zNjyXPyWLwrNqpIVr6wE"
+    BOT_TOKEN = "5615309337:AAH0kYcdUQD7mdSlMYshlC_ESKnR6qq7kxA"
     DEV = "7556932"
     OWNER = "7556932"
     ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By AnshuSharma (https://github.com/Anshusharma75/TG-videoCompress)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
